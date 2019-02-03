38cdced453489406553f31fc24c8e33d277be7f7
Ekay Ancurtotal <41782306+ancurtotal@users.noreply.github.com>
2019-02-03 20:17:26 +0700

    Add files via upload

--- ancurtotal.py
+++ ancurtotal.py
@@ -15,7 +15,7 @@ def main():
 		pwd = raw_input("password : ")
 
 		if pwd == password:
-			print "\n\033[1;34mHello Welcome To Ancurtotal Spammer Tools", 
+			print "\n\033[1;34mHello Welcome To Ancurtotal Hacking Wifi Tools", 
 			sys.exit()
 
 		else:
